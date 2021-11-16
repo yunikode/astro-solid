3fc483bcc46876c94011a63f9d0afababfe37e4a
zee-german <franz.lutzenberger@gmail.com>
2021-11-16 16:02:42 +0000

    Deploying to gh-pages from @ zee-german/astro-solid@665ac48bdca1ca3fdaa3ba4a1f00b9cc2ca934e2 🚀

---
+++ assets/client.8ae49716.js
@@ -0,0 +1,1 @@
+import{h as o,a as c}from"./vendor.786e7115.js";var s=t=>(r,a,n)=>{const e=document.createElement("astro-fragment");e.innerHTML=n,o(()=>c(r,{...a,children:e}),t)};export{s as default};
